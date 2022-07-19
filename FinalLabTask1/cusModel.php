f67548721650b65af582f697cd57bde41f4416f6
Rahamatullah Sun <RahamatullahSun1@gmail.com>
2022-07-19 13:44:48 +0600

    FinalLabTask1

---
+++ FinalLabTask1/cusModel.php
@@ -0,0 +1,21 @@
+<?php
+
+        function getAllUser(){
+
+            $conn = getconnection();
+            $sql = "select * from users";
+            $result = mysqli_query($conn, $sql);
+            $count = mysqli_num_rows($result);
+
+            if($count > 0){
+                while($data = mysqli_fetch_assoc($result)){
+                    print_r($data);
+                    echo "<br>";
+                }
+            }else{
+                echo 'No Data Found!!!';
+            }
+
+        }
+
+?>
